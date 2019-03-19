2853ae8b1b3650f1334e2deb3915e04a8ac03caf
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-19 17:53:12 -0400

    Added cors. Not sure if my import syntax is correct, but I'll know soon enough.

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const dbRouter = require('./data/db-router.js');
 
 server.use(require('express').json());
 
+// Possible problem here:
+server.use(require('cors')());
+
 server.use('/api/posts', dbRouter);
 
 server.get('/', (req, res) => {
