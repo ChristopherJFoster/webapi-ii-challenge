ff3f031de73b4642f759eb9fb2d5f7fee1f92df5
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-19 17:12:43 -0400

    DELETE single post works. DELETE invalid post id throws error correctly.

--- data/db-router.js
+++ data/db-router.js
@@ -34,8 +34,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  db.findById(id)
+  db.findById(req.params.id)
     .then(post => {
       if (post[0]) {
         res.status(200).json(post[0]);
@@ -49,6 +48,22 @@ router.get('/:id', (req, res) => {
       res
         .status(500)
         .json({ error: 'The post information could not be retrieved.' });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  db.findById(req.params.id)
+    .then(post => {
+      if (post[0]) {
+        db.remove(req.params.id).then(res.status(200).json(post[0]));
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'The post could not be removed' });
     });
 });
 
