ba2fb08ebbd42796a79f2985064554a1109ac9e8
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-19 16:06:11 -0400

    Set up express router db-router.js, along with import in server.js. I am able to GET the posts array correctly. I'm unsure of the best way to text the catch portion of the code.

---
+++ data/db-router.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+const db = require('./db.js');
+
+router.get('/', (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The posts information could not be retrieved.' });
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const server = require('express')();
 
+const dbRouter = require('./data/db-router.js');
+
 server.use(require('express').json());
 
+server.use('/api/posts', dbRouter);
+
 server.get('/', (req, res) => {
-  res.send('Server is running...');
+  res.send('Server running...');
 });
 
 module.exports = server;
