67c0887fd5bac1208aedc2dff0fb1ff0e2bce892
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-19 16:59:42 -0400

    GET single post works. GET invalid post id throws error correctly.

--- data/db-router.js
+++ data/db-router.js
@@ -33,4 +33,23 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  db.findById(id)
+    .then(post => {
+      if (post[0]) {
+        res.status(200).json(post[0]);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The post information could not be retrieved.' });
+    });
+});
+
 module.exports = router;
