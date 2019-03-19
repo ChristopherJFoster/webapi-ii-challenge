bb38c96012f2488e6c22e6ee944d5b3f4876b820
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-19 16:39:32 -0400

    POSTing a complete post works correctly. POSTing an incomplete throws the correct error message.

--- data/db-router.js
+++ data/db-router.js
@@ -2,6 +2,25 @@ const router = require('express').Router();
 
 const db = require('./db.js');
 
+router.post('/', (req, res) => {
+  const newPost = req.body;
+  if (newPost.title && newPost.contents) {
+    db.insert(newPost)
+      .then(post => {
+        res.status(201).json(newPost);
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: 'There was an error while saving the post to the database'
+        });
+      });
+  } else {
+    res.status(400).json({
+      errorMessage: 'Please provide title and contents for the post.'
+    });
+  }
+});
+
 router.get('/', (req, res) => {
   db.find()
     .then(posts => {
