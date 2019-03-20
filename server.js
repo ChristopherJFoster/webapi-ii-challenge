7903acafbb9efc8bc2d25c96497c2a147eae7687
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-20 09:32:53 -0400

    Added custom logger middleware example from Express Middleware TK.

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const dbRouter = require('./data/db-router.js');
 
 server.use(require('express').json());
 
-// Possible problem here:
+// Logger example from Express Middleware TK:
+server.use(logger);
+
+// Possible problem with syntax here:
 server.use(require('cors')());
 
 server.use('/api/posts', dbRouter);
@@ -14,3 +17,13 @@ server.get('/', (req, res) => {
 });
 
 module.exports = server;
+
+// Logger example from Express Middleware TK:
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'Origin'
+    )}`
+  );
+  next();
+}
